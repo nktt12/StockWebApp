9d1686c122e4b5ee8381868ba0c0329c90cb8e1b
Nikhil Karun <nikhil.karun@chch.ox.ac.uk>
2025-08-02 22:17:23 +0100

    adding libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import root_mean_squared_error
